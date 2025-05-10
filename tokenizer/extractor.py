6e0187f57e9f2b5c706676bfa4c53765c9888c2d
gnatykdm <gnatykwork@gmail.com>
2025-05-10 20:51:25 +0200

    b2t-tokenizer-skeleton C:1

---
+++ tokenizer/extractor.py
@@ -0,0 +1,13 @@
+
+class ByteExtractor:
+    @classmethod
+    def from_text(text: str) -> str:
+        pass
+    @classmethod
+    def from_img(img_path: str) -> str:
+        pass
+    @classmethod
+    def from_audio(audio_path: str) -> str:
+        pass
+    def from_file(file_path: str) -> str:
+        pass
